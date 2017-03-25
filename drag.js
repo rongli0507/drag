46d7df0c163f2c1a33a392421b8d6a503f2e4613
chenzihao111 <446475587@qq.com>
2017-03-25 13:45:19 +0800

    tj demo2 and drag.js

--- drag.js
+++ drag.js
@@ -7,5 +7,13 @@ function drag(id){
     obj.onmousedown = function(argument) {
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		};		
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		};		
     }
 }
