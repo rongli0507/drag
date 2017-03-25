abfb5b2894746dcf87e26a6fcd51abc65d2a9259
rongli0507 <527604860@qq.com>
2017-03-25 20:28:26 +0800

    rongli changed 1

--- drag.js
+++ drag.js
@@ -1,19 +1,20 @@
 // JavaScript Document
-
+// rongli0507
 function drag(id){
-    var obj = Document.getElementById(id);
-    var disX = 0;
+	var obj = document.getElementById(id);
+	var disX = 0;
 	var disY = 0;
-    obj.onmousedown = function(argument) {
+	obj.onmousedown = function(ev){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
 		document.onmousemove = function(ev){
 			obj.style.left = ev.pageX - disX + 'px';
 			obj.style.top = ev.pageY - disY + 'px';
-		};		
+		};
 		document.onmouseup = function(){
 			document.onmousemove = null;
 			document.onmouseup = null;
-		};		
-    }
+		};
+	}
+	return false;
 }
